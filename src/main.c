9788983b8cd9c41c7bf2556bcf36c42cbb57785e
jackr276 <jack_m_robbins@me.com>
2024-08-31 16:15:10 -0400

    restructure and add tester file

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jack Robbins
+ * This is an example program that will demonstrate the functionality of the regex library
+ */
+
+#include <stdio.h>
+#include "regex.h"
+
+int main(){
+	//Grab the regex
+	char regex[100];
+	printf("Enter the regex:");
+	scanf("%s", regex);
+
+
+}
