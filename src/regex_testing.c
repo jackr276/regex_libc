76fb51b21743915ed806b8e4b69bc807628a8996
jackr276 <jack_m_robbins@me.com>
2024-12-29 08:53:17 -0500

    Add email validation example

--- src/regex_testing.c
+++ src/regex_testing.c
@@ -2072,7 +2072,7 @@ void test_case_run(u_int8_t test_case){
 	
 			return;
 
-		//Currently causing issues
+		//Working
 		case 75:
 			printf("Testing filename matching\n");
 			printf("Regex: ($*).(c|h)\n");
@@ -2099,7 +2099,32 @@ void test_case_run(u_int8_t test_case){
 	
 			return;
 
+		//Email address
+		case 76:
+			printf("Testing filename matching\n");
+			printf("Regex: ($*)@($*).((com)|(edu))\n");
+	
+			//IDEA -- add a "dummy" or "epsilon" state in between a +/*/? and ()'s
+			tester = define_regular_expression("($*)@($*).((com)|(edu))", REGEX_VERBOSE);
 
+			//Define a test string
+			test_string = "jack_m_robbins@me.com";
+			printf("TEST STRING: %s\n\n", test_string);
+			
+			//Test the matching
+			regex_match(tester, &matcher, test_string, 0, REGEX_VERBOSE);
+
+			//Define a test string -- should work 
+			test_string = "noreply@njit.edu";
+			printf("TEST STRING: %s\n\n", test_string);
+			
+			//Test the matching
+			regex_match(tester, &matcher, test_string, 0, REGEX_VERBOSE);
+
+			//Destroy the regex
+			destroy_regex(tester);
+	
+			return;
 
 		//Added to avoid comptime errors, we shouldn't reach this
 		default:
@@ -2120,7 +2145,7 @@ int main(int argc, char** argv){
 	
 	//Run them all
 	if(argc == 1){
-		for(u_int8_t i = 0; i < 75; i++){
+		for(u_int8_t i = 0; i < 77; i++){
 			test_case_run(i);
 		}
 	}
